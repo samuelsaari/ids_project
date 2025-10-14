9071bd01ce9b97cc6e8127b1894bd2fdd78f1c15
Sanni Isokirmo <sanni.isokirmo@gmail.com>
2025-10-14 03:04:15 +0300

    Add charts with dummy data

---
+++ bggrecommenderapp/static/chart.js
@@ -0,0 +1,24 @@
+function initChart(recId, recRatings) {
+  const canvas = document.getElementById(recId);
+  const ctx = canvas.getContext('2d');
+
+  const labels = Object.keys(recRatings);
+  const values = Object.values(recRatings);
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        data: values,
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  });
+}
