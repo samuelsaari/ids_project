a299557ca1b923d373ec5297a819130e25c46fea
Ahti Katiska <unconfigured@null.spigotmc.org>
2025-10-23 13:51:15 +0300

    add loading screen

--- bggrecommenderapp/__init__.py
+++ bggrecommenderapp/__init__.py
@@ -2,13 +2,58 @@ import os
 
 from flask import Flask, request, render_template, session, redirect, url_for, jsonify
 
+import sqlite3, threading, os, uuid
+
 from . import data_handler as D
 from . import recommending as R
 from . import util as U
 
+DB_PATH = "jobs.db"
+
+
+data_handler = D.DataHandler()
+
+
+def init_db():
+    if not os.path.exists(DB_PATH):
+        with sqlite3.connect(DB_PATH) as conn:
+            conn.execute(
+                """
+                CREATE TABLE jobs (
+                    id TEXT PRIMARY KEY,
+                    status TEXT
+                )
+            """
+            )
+
+
+def set_job(job_id, status):
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.execute(
+            "REPLACE INTO jobs (id, status) VALUES (?, ?)",
+            (job_id, status),
+        )
+        conn.commit()
+
+
+def get_job(job_id):
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.execute("SELECT id, status FROM jobs WHERE id=?", (job_id,))
+        row = cur.fetchone()
+        return {"id": row[0], "status": row[1]} if row else None
+
+
+def fetch_and_calc_rec_matrix_async(job_id, username):
+    bgg_data = data_handler.fetch_new_user_into_bgg_data(username)
+    rec_mat = R.generate_recommendation_matrix(bgg_data)
+    data_handler.set_rec_mat(rec_mat)
+    set_job(job_id, "done")
+
 
 def create_app(test_config=None):
     # create and configure the app
+
+    init_db()
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
@@ -27,8 +72,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    data_handler = D.DataHandler()
-
     if data_handler.get_rec_mat() is None:
         # Calc nmf in advance, so that first user doesn't have to wait
 
@@ -44,15 +87,35 @@ def create_app(test_config=None):
 
             if username not in data_handler.get_bgg_data().columns:
 
-                bgg_data = data_handler.fetch_new_user_into_bgg_data(username)
+                job_id = str(uuid.uuid1())
 
-                # Regenerate the recommendation matrix
-                rec_mat = R.generate_recommendation_matrix(bgg_data)
-                data_handler.set_rec_mat(rec_mat)
+                set_job(job_id, "working")
+
+                thread = threading.Thread(
+                    target=fetch_and_calc_rec_matrix_async, args=(job_id, username)
+                )
+                thread.start()
+
+                return redirect(url_for("loading", job_id=job_id))
 
             return redirect(url_for("recommendations"))
 
         return render_template("index.html")
+
+    @app.route("/loading/<job_id>")
+    def loading(job_id):
+        return render_template("loading.html", job_id=job_id)
+
+    @app.route("/check_status/<job_id>")
+    def check_status(job_id):
+        job = get_job(job_id)
+        if not job:
+            return jsonify({"done": False})
+
+        if job["status"] == "done":
+            return jsonify({"done": True})
+
+        return jsonify({"done": False})
 
     @app.route("/usernames")
     def usernames():
